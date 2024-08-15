b87fd2158cdedeff537aaef45d26d5b5857551eb
Matt Hale <matthew.hale@protonmail.com>
2024-08-15 16:09:13 -0400

    Add more complex test for getting ability bonuses

--- rerole-lib/tests/ability/test_ability.py
+++ rerole-lib/tests/ability/test_ability.py
@@ -1,4 +1,5 @@
 from rerole_lib import ability
+from rerole_lib import bonus
 
 def test_ability_modifier_calculation():
     scores = [
@@ -10,3 +11,37 @@ def test_ability_modifier_calculation():
 
     for i, s in enumerate(scores):
         assert ability.modifier(s) == modifiers[i]
+
+def test_complex_calculation():
+    strength = {
+        "value": 15,
+        "damage": 3,
+        "drain": 1
+    }
+    belt_of_strength = {
+        "value": 3,
+        "type": bonus.type.ENHANCEMENT
+    }
+
+    # Ability drain should effectively reduce the strength score by 1; with the +3 from 
+    # the belt, the effective strength modifier should be +3.
+    #
+    # Ability _damage_ does not change the calculation of the ability modifier 
+    # directly; rather, for every 2 points of damage, anything utilizing that ability 
+    # score suffers a penalty of -1
+    #
+    # Ergo:
+    bonuses_should_be = [
+        {
+            "value": 3
+        },
+        {
+            "value": -1
+        }
+    ]
+    bonuses_are = ability.to_bonuses(
+        ability=strength,
+        bonuses=[belt_of_strength]
+    )
+
+    assert bonuses_should_be == bonuses_are
