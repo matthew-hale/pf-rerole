53f32006e1cde0508ec0aa8b7d07144aa038d35c
Matt Hale <matthew.hale@protonmail.com>
2024-08-16 21:06:59 -0400

    Implement character.roll_skill function

--- rerole-lib/rerole_lib/character.py
+++ rerole-lib/rerole_lib/character.py
@@ -1,6 +1,7 @@
 import json
 
 from rerole_lib import ability
+from rerole_lib import effect
 from rerole_lib import utils
 
 def load(filepath):
@@ -67,6 +68,35 @@ def build_effect_index(data: dict) -> dict | None:
             utils.add_or_append(effect_index, n, key_seq)
 
     return effect_index
+
+def roll_skill(data: dict, effect_index: dict, skill_name: str) -> int | None:
+    """Calculate the final modifier of the provided skill name."""
+    skill_data = utils.get_in(data, ["skills", skill_name])
+    if not skill_data:
+        return None
+
+    ability_name = skill_data.get("ability", "")
+    ability_as_effects = ability_to_effects(
+        data=data,
+        effect_index=effect_index,
+        ability_name=ability_name,
+    )
+    if not ability_as_effects:
+        return None
+
+    ranks = skill_data.get("ranks", 0)
+
+    is_class_skill = skill_data.get("class", False)
+    has_ranks = ranks > 0
+    should_receive_class_skill_bonus = is_class_skill and has_ranks
+    class_skill_bonus = 3 if should_receive_class_skill_bonus else 0
+
+    skill_effect_keys = effect_index.get(skill_name, [[]])
+    skill_effects = [utils.get_in(data, ks) for ks in skill_effect_keys]
+
+    all_effects = ability_as_effects + skill_effects
+
+    return ranks + class_skill_bonus + effect.total(all_effects)
 
 def ability_to_effects(data: dict, effect_index: dict, ability_name: str) -> list | None:
     """Apply all relevant effects to the specified ability, then convert that ability to a list of effects to be applied to something else."""
