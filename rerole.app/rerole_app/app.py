a1e16a9d432befc518c6c0f6dbebd4834de4eb1b
Matt Hale <matthew.hale@protonmail.com>
2024-08-21 12:11:24 -0400

    Add api and database init to main flask app

--- rerole.app/rerole_app/app.py
+++ rerole.app/rerole_app/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
+from rerole_app.api import api
+from rerole_app.db import init
 from rerole_app.site import site
 
 app = Flask(__name__)
 app.register_blueprint(site)
+app.register_blueprint(api)
 
 app.json.sort_keys = False
+
+init()
