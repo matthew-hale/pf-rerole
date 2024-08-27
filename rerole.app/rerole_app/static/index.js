ff4b974ee3ab0f690eeaada1396b5723a81e1b7a
Matt Hale <matthew.hale@protonmail.com>
2024-08-27 13:12:32 -0400

    Overhaul main site landing page to use new auth

--- rerole.app/rerole_app/static/index.js
+++ rerole.app/rerole_app/static/index.js
@@ -8,47 +8,40 @@ window.onload = populate_list;
 
 async function populate_list() {
     const dom_character_list = document.getElementById("character-list");
-    try {
-        const character_list = await get_character_list();
-        console.log("success");
-        for (const c of character_list) {
-            let id = c.id;
-            let name = c.name ? c.name : "Unnamed character";
+    const character_list = await get_character_list()
+    for (const c of character_list) {
+        let id = c.id;
+        let name = c.name ? c.name : "Unnamed character";
 
-            let c_div = document.createElement("div");
-            c_div.setAttribute("class", "character");
-            c_div.setAttribute("id", id);
+        let c_div = document.createElement("div");
+        c_div.setAttribute("class", "character");
+        c_div.setAttribute("id", id);
 
-            let h3 = document.createElement("h3");
-            h3.innerHTML = name;
-            c_div.appendChild(h3);
+        let h3 = document.createElement("h3");
+        h3.innerHTML = name;
+        c_div.appendChild(h3);
 
-            let buttons = document.createElement("div");
-            buttons.setAttribute("class", "buttons");
-            c_div.appendChild(buttons);
+        let buttons = document.createElement("div");
+        buttons.setAttribute("class", "buttons");
+        c_div.appendChild(buttons);
 
-            let link = document.createElement("a");
-            link.setAttribute("href", `${site_characters_url}/${id}`);
-            link.innerHTML = "View";
-            buttons.appendChild(link);
+        let link = document.createElement("a");
+        link.setAttribute("href", `${site_characters_url}/${id}`);
+        link.innerHTML = "View";
+        buttons.appendChild(link);
 
-            let delete_button = document.createElement("button");
-            delete_button.setAttribute("onclick", "delete_character(this)");
-            delete_button.setAttribute("type", "button");
-            delete_button.setAttribute("class", "delete");
-            delete_button.innerHTML = "Delete";
-            buttons.appendChild(delete_button);
+        let delete_button = document.createElement("button");
+        delete_button.setAttribute("onclick", "delete_character(this)");
+        delete_button.setAttribute("type", "button");
+        delete_button.setAttribute("class", "delete");
+        delete_button.innerHTML = "Delete";
+        buttons.appendChild(delete_button);
 
-            dom_character_list.appendChild(c_div);
-        }
-    } catch (error) {
-        alert("An error occurred while fetching your list of characters.");
-        console.error(error);
-        window.location.reload();
+        dom_character_list.appendChild(c_div);
     }
 }
 
-async function delete_character(obj) {
+function delete_character(obj) {
     const buttons = obj.parentNode;
     const c_div = buttons.parentNode;
     const id = c_div.getAttribute("id");
@@ -56,27 +49,47 @@ async function delete_character(obj) {
         return
     }
     try {
-        const response = await fetch(`${base_api_url}/characters/${id}`, {
+        fetch(`${base_api_url}/characters/${id}`, {
             method: "DELETE",
-            credentials: "same-origin"
-        });
-        if (response.ok) {
-            c_div.remove();
-        }
+            headers: {
+                Authorization: get_authorization_header(),
+            },
+            credentials: "same-origin",
+        })
+            .then((res) => {
+                if (res.status == 401) {
+                    logout();
+                }
+                if (res.ok) {
+                    c_div.remove();
+                }
+            });
     } catch (error) {
         alert("An error occurred while deleting your character.");
         console.error(error);
     }
 }
 
-async function create_character() {
+function create_character() {
     try {
-        const response = await fetch(`${base_api_url}/characters`, {
+        let data;
+        fetch(`${base_api_url}/characters`, {
             method: "POST",
+            headers: {
+                Authorization: get_authorization_header(),
+            },
             credentials: "same-origin"
-        });
-        const data = await response.json();
-        window.location.href = `${site_characters_url}/${data.id}`;
+        })
+            .then((res) => {
+                if (res.status == 401) {
+                    logout();
+                }
+                return res.json()
+            })
+            .then((json) => {
+                data = json;
+                window.location.href = `${site_characters_url}/${data.id}`;
+            });
     } catch (error) {
         alert("An error occurred while creating a new character.");
         console.error(error);
@@ -86,10 +99,45 @@ async function create_character() {
 
 async function get_character_list() {
     const endpoint = `${base_api_url}/characters`;
-    const response = await fetch(endpoint, {
+    let headers = {
+        Authorization: get_authorization_header()
+    }
+    return await fetch(endpoint, {
         method: "GET",
+        headers: headers,
         credentials: "same-origin"
-    });
-    const list = await response.json();
-    return list;
+    })
+        .then((res) => {
+            if (res.status == 401) {
+                throw "401";
+            }
+            return res.json();
+        })
+        .catch((err) => {
+            logout();
+        });
+}
+
+function logout() {
+    let token = window.localStorage.getItem("token");
+    fetch(`${base_api_url}/logout`, {
+        method: "POST",
+        headers: {
+            Authorization: get_authorization_header(),
+        },
+        credentials: "same-origin",
+    })
+        .then((res) => {
+            console.log(res);
+        })
+        .finally(() => {
+            window.localStorage.clear();
+            window.location.replace(`${hostname}/logout`);
+        });
+}
+
+function get_authorization_header() {
+    let token = window.localStorage.getItem("token");
+    let value = `Bearer ${token}`;
+    return value;
 }
