aca6a83704ceb496d82d0cfaff4bd852739ea163
Matt Hale <matthew.hale@protonmail.com>
2024-08-31 22:02:16 -0400

    Also move .get_in to separate function

--- rerole-lib/rerole_lib/utils.py
+++ rerole-lib/rerole_lib/utils.py
@@ -3,86 +3,11 @@ from functools import reduce
 class Dict(dict):
     """Provides some utility methods on standard dictionaries that I find helpful."""
     def get_in(self, key_seq: list, default=None):
-        """A la Clojure's `get-in`; like .get, but uses a sequence of keys.
-
-        This function mimics Clojure's `get-in` function. Provide it with a sequence of keys and an optional default value, and it will return either the specified nested value, or the default.
-
-        Example:
-
-            data = Dict({
-                "a": {
-                    1: {
-                        "apple": "tasty",
-                    },
-                },
-                "b": [
-                    {"bagel": "everything"},
-                    {"bagel": "asiago"},
-                    {"bagel": "plain"},
-                    {"bagel": "sesame seed"},
-                ]
-            })
-            data.get_in(["a", 1, "apple"])
-            # 'tasty'
-
-            data.get_in(["a", 1])
-            # {'apple': 'tasty'}
-
-            data.get_in(["a", 1, "banana"]) # returns None
-            data.get_in(["a", 2, "banana"]) # returns None
-            data.get_in(["a", 2])           # returns None
-            data.get_in(["b"])              # returns None
-
-            data.get_in(["a", 1, "banana"], default={})
-            # {}
-
-        Note that Python's dict and list types do not implement a common interface for a safe "get" operation. Whereas in Clojure, a call to `get-in` on a list of dicts of lists of dicts would "just work," it would not normally work in Python.
-
-        It does, however, work here, because I made it so:
-
-            data.get_in(["b", 1, "bagel"])
-            # "asiago"
-
-        The default value of `None` for the `default` parameter makes `get_in` behave like Python's own `.get()` method for dictionaries. It would have been more personally useful to set the default to `{}`, but I wanted to stick to the normal language behavior as much as possible.
-
-        One scenario that this function trips up on is when `None` is a possible valid value. I think it should basically never be a valid value, so I don't really care to fix this, but beware:
-
-            data["a"][1]["cucumber"] = None
-            data.get_in(["a", 1, "cucumber"], default="pickles")
-            # 'pickles'
-        """
-        if not key_seq:
-            return default
-
-        def getter(data, key):
-            if data is None:
-                return None
-            try:
-                keys = data.keys()
-            except AttributeError:
-                # Might be a list
-                pass
-            else:
-                # Is a dict
-                if key not in keys:
-                    return None
-                return data.get(key)
-
-            try:
-                return data[key]
-            except:
-                # Either isn't a list, or the index was out of range
-                return None
-
-        output = reduce(getter, key_seq, self)
-        if output is None:
-            return default
-        return output
+        """Wraps `utils.get_in()`"""
+        return get_in(self, key_seq, default)
 
     def search(self, fn, path=[]) -> list | None:
-        """Provides `utils.search` as a Dict method."""
-        if not self:
-            return None
+        """Wraps `utils.search`"""
         return search(self, fn)
 
     def add_or_append(self, key, val):
@@ -94,6 +19,83 @@ class Dict(dict):
             self[key] = []
         self[key].append(val)
 
+
+def get_in(data, key_seq, default=None):
+    """A la Clojure's `get-in`; like .get, but uses a sequence of keys.
+
+    This function mimics Clojure's `get-in` function. Provide it with a sequence of keys and an optional default value, and it will return either the specified nested value, or the default.
+
+    Example:
+
+        data = {
+            "a": {
+                1: {
+                    "apple": "tasty",
+                },
+            },
+            "b": [
+                {"bagel": "everything"},
+                {"bagel": "asiago"},
+                {"bagel": "plain"},
+                {"bagel": "sesame seed"},
+            ]
+        })
+        get_in(data, ["a", 1, "apple"])
+        # 'tasty'
+
+        get_in(data, ["a", 1])
+        # {'apple': 'tasty'}
+
+        get_in(data, ["a", 1, "banana"]) # returns None
+        get_in(data, ["a", 2, "banana"]) # returns None
+        get_in(data, ["a", 2])           # returns None
+        get_in(data, ["b"])              # returns None
+
+        get_in(data, ["a", 1, "banana"], default={})
+        # {}
+
+    Note that Python's dict and list types do not implement a common interface for a safe "get" operation. Whereas in Clojure, a call to `get-in` on a list of dicts of lists of dicts would "just work," it would not normally work in Python.
+
+    It does, however, work here, because I made it so:
+
+        get_in(data, ["b", 1, "bagel"])
+        # "asiago"
+
+    The default value of `None` for the `default` parameter makes `get_in` behave like Python's own `.get()` method for dictionaries. It would have been more personally useful to set the default to `{}`, but I wanted to stick to the normal language behavior as much as possible.
+
+    One scenario that this function trips up on is when `None` is a possible valid value. I think it should basically never be a valid value, so I don't really care to fix this, but beware:
+
+        data["a"][1]["cucumber"] = None
+        get_in(data, ["a", 1, "cucumber"], default="pickles")
+        # 'pickles'
+    """
+    if not data or not key_seq:
+        return default
+
+    def getter(data, key):
+        if data is None:
+            return None
+        try:
+            keys = data.keys()
+        except AttributeError:
+            # Might be a list
+            pass
+        else:
+            # Is a dict
+            if key not in keys:
+                return None
+            return data.get(key)
+
+        try:
+            return data[key]
+        except:
+            # Either isn't a list, or the index was out of range
+            return None
+
+    output = reduce(getter, key_seq, data)
+    if output is None:
+        return default
+    return output
 
 def search(data, fn, path=[]):
     """Return a list of matching key sequences in `data`, where fn(data.get_in(seq)) is True.
