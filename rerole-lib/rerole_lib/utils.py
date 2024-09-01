6b9c96fdd45a4a54bb51d9efdac06ecd5dbb45ee
Matt Hale <matthew.hale@protonmail.com>
2024-08-31 21:18:11 -0400

    Update Dict.get_in to work with list indexes as well

--- rerole-lib/rerole_lib/utils.py
+++ rerole-lib/rerole_lib/utils.py
@@ -15,6 +15,12 @@ class Dict(dict):
                         "apple": "tasty",
                     },
                 },
+                "b": [
+                    {"bagel": "everything"},
+                    {"bagel": "asiago"},
+                    {"bagel": "plain"},
+                    {"bagel": "sesame seed"},
+                ]
             })
             data.get_in(["a", 1, "apple"])
             # 'tasty'
@@ -32,6 +38,11 @@ class Dict(dict):
 
         Note that Python's dict and list types do not implement a common interface for a safe "get" operation. Whereas in Clojure, a call to `get-in` on a list of dicts of lists of dicts would "just work," it would not normally work in Python.
 
+        It does, however, work here, because I made it so:
+
+            data.get_in(["b", 1, "bagel"])
+            # "asiago"
+
         The default value of `None` for the `default` parameter makes `get_in` behave like Python's own `.get()` method for dictionaries. It would have been more personally useful to set the default to `{}`, but I wanted to stick to the normal language behavior as much as possible.
 
         One scenario that this function trips up on is when `None` is a possible valid value. I think it should basically never be a valid value, so I don't really care to fix this, but beware:
@@ -46,9 +57,22 @@ class Dict(dict):
         def getter(data, key):
             if data is None:
                 return None
-            if key not in data:
+            try:
+                keys = data.keys()
+            except AttributeError:
+                # Might be a list
+                pass
+            else:
+                # Is a dict
+                if key not in keys:
+                    return None
+                return data.get(key)
+
+            try:
+                return data[key]
+            except:
+                # Either isn't a list, or the index was out of range
                 return None
-            return data.get(key)
 
         output = reduce(getter, key_seq, self)
         if output is None:
